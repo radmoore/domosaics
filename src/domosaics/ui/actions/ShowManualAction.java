185a500a2ff094283e2949c673fc64ee9d7d65d1
Andrew Moore <radmoore@gmail.com>
2012-12-30 00:55:55 +0100

    added manual action

--- src/domosaics/ui/actions/ShowManualAction.java
+++ src/domosaics/ui/actions/ShowManualAction.java
@@ -3,21 +3,40 @@ package domosaics.ui.actions;
 import java.awt.event.ActionEvent;
 
 import domosaics.ui.io.menureader.AbstractMenuAction;
+import domosaics.ui.util.MessageUtil;
 
 
 
 
 /**
- * Opens the DoMosaicS manual
+ * Opens the DoMosaics Documentation website
  * 
- * @author Andreas Held
+ * 
+ * @author Andrew D. Moore <radmoore@uni-muenster.de>
  *
  */
 public class ShowManualAction extends AbstractMenuAction{
 	private static final long serialVersionUID = 1L;
+	private static final String DOMOSDOCS = "http://domosdocs.uni-muenster.de";
 
 	public void actionPerformed(ActionEvent e) {
-//		new ManualFrame().setVisible(true);
-	}
+
+        if( !java.awt.Desktop.isDesktopSupported() ) {
+        	MessageUtil.showWarning("The website could not be opened. Navigate to "+ShowManualAction.DOMOSDOCS+ "manually.");
+        }
+
+        java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
+
+        if( !desktop.isSupported( java.awt.Desktop.Action.BROWSE ) ) {
+        	MessageUtil.showWarning("The website could not be opened. Navigate to "+ShowManualAction.DOMOSDOCS+ "manually.");
+        }
+        try {
+        	java.net.URI uri = new java.net.URI( ShowManualAction.DOMOSDOCS );
+            desktop.browse( uri );
+        }
+        catch ( Exception excp ) {
+        	System.err.println( excp.getMessage() );
+         }
+    }
 
 }
