ce7c060b4aa7d679d83708a8785552fd275437d5
Nicolas Terrapon <terrapon@sencha.(none)>
2013-02-18 23:13:07 +0100

    Authorize but remove any white space in edited sequences First test on short-cut to load fasta files (with iconified button)

---
+++ src/domosaics/ui/wizards/dialogs/LoadFastaDialog.java
@@ -0,0 +1,121 @@
+package domosaics.ui.wizards.dialogs;
+
+import java.awt.EventQueue;
+import java.io.File;
+import java.util.Map;
+
+import org.netbeans.api.wizard.WizardDisplayer;
+import org.netbeans.spi.wizard.ResultProgressHandle;
+import org.netbeans.spi.wizard.Wizard;
+import org.netbeans.spi.wizard.WizardException;
+import org.netbeans.spi.wizard.WizardPage;
+import org.netbeans.spi.wizard.WizardPage.WizardResultProducer;
+
+import domosaics.model.configuration.Configuration;
+import domosaics.model.sequence.SequenceI;
+import domosaics.model.sequence.io.FastaReader;
+import domosaics.model.workspace.ProjectElement;
+import domosaics.ui.ViewHandler;
+import domosaics.ui.WorkspaceManager;
+import domosaics.ui.views.ViewType;
+import domosaics.ui.views.domaintreeview.DomainTreeView;
+import domosaics.ui.views.domainview.DomainView;
+import domosaics.ui.views.sequenceview.SequenceView;
+import domosaics.ui.views.treeview.TreeView;
+import domosaics.ui.views.view.io.ViewImporter;
+import domosaics.ui.wizards.pages.ImportViewPage;
+import domosaics.ui.wizards.pages.LoadFastaPage;
+
+
+
+
+/**
+ * Wizard dialog asking the user for a name to assign to a specified 
+ * object. This object can be a project within the workspace or a view.
+ * 
+ * @author Andrew D. Moore <radmoore@uni-muenster.de>
+ *
+ */
+public class LoadFastaDialog {
+
+	/** the default name */
+	protected String defaultName = null;
+	
+	/** the object to name */
+	protected String objectName = null;
+	
+	protected boolean allowProjectSelection; 
+	
+	/**
+	 * Constructor for a new SelectViewNameDialog
+	 * 
+	 * @param defaultName
+	 * 		the default name
+	 * @param objectName
+	 * 		the object to name e.g. view or project
+	 */
+	public LoadFastaDialog() {
+	}
+	
+	/**
+	 * Shows the wizard
+	 * 
+	 * @return
+	 * 		the chosen name
+	 */
+	public Object show() {
+		Wizard wiz = WizardPage.createWizard(new WizardPage[]{new LoadFastaPage()}, new LoadFastaProgress());
+		return WizardDisplayer.showWizard(wiz);				 
+	}
+}
+
+class LoadFastaProgress implements WizardResultProducer{
+	
+	public void start(Map m, ResultProgressHandle p) {
+		assert !EventQueue.isDispatchThread();
+	}
+	
+	public boolean cancel(Map m) {
+		return true;
+	}
+	
+	public Object finish(Map m) throws WizardException {
+
+		// display view after import
+		//ViewImporter.displayViewAfterImport(true);
+		
+		
+		// create a new view
+		try {
+			
+			String projectName = (String) m.get(ImportViewPage.PROJECTNAME_KEY);
+			String viewFilePath = (String) m.get(ImportViewPage.FILEPATH_KEY);
+			String viewName = (String) m.get(ImportViewPage.VIEWNAME_KEY);
+
+			//System.out.println("Importing viewtype: "+viewType);
+			
+			ProjectElement project = WorkspaceManager.getInstance().getProject(projectName);
+			File viewFile = new File(viewFilePath);
+			
+			SequenceView seqView = ViewHandler.getInstance().createView(ViewType.SEQUENCE, viewName);
+			
+			// Adapted by Nico
+			SequenceI[] seqs = new FastaReader().getDataFromFile(viewFile);;
+			seqView.setSeqs(seqs);
+			
+			ViewHandler.getInstance().addView(seqView, project, false);
+			//ViewImporter.readSequenceView(viewFile, project, viewName);
+		} 
+		catch (Exception e) {
+			Configuration.getLogger().debug(e.toString());
+			e.printStackTrace();
+		}
+		
+		// set back to default
+		ViewImporter.displayViewAfterImport(false);
+		
+		return null;
+	}	
+}
+
+
---
+++ src/domosaics/ui/wizards/pages/LoadFastaPage.java
@@ -0,0 +1,157 @@
+package domosaics.ui.wizards.pages;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
+
+import org.jdesktop.swingx.JXTitledSeparator;
+import org.netbeans.spi.wizard.WizardPage;
+
+import domosaics.model.sequence.SequenceI;
+import domosaics.model.sequence.io.FastaReader;
+import domosaics.model.workspace.ProjectElement;
+import domosaics.ui.DoMosaicsUI;
+import domosaics.ui.ViewHandler;
+import domosaics.ui.WorkspaceManager;
+import domosaics.ui.util.FileDialogs;
+import domosaics.ui.util.MessageUtil;
+import domosaics.ui.views.ViewType;
+import domosaics.ui.views.domainview.DomainView;
+import domosaics.ui.views.sequenceview.SequenceView;
+import domosaics.ui.views.view.io.ViewImporter;
+
+
+
+
+/**
+ * WizardPage shown within the SelectViewNameDialog.
+ * 
+ * @author Andrew D. Moore <radmoore@uni-muenster.de>
+ *
+ */
+public class LoadFastaPage extends WizardPage {
+	private static final long serialVersionUID = 1L;
+	
+	public static final String FILEPATH_KEY = "filePath";
+	public static final String VIEWNAME_KEY = "viewName";
+	public static final String PROJECTNAME_KEY = "projectName";
+
+	private JTextField name, path;
+	private JComboBox selectProject;
+	private JButton browse;
+	
+	private ViewType type=ViewType.SEQUENCE;
+
+	/** the check for correct fasta format */
+	protected SequenceI[] seqs = null;
+	
+	/**
+	 * Constructor for a new SelectViewNamePage
+	 * 
+	 * @param defaultName
+	 * 		the default name for the object
+	 * @param objectName
+	 * 		the object to name e.g. view or project
+	 */
+	public LoadFastaPage() {
+		super("Load fasta to project");
+		setLayout(new MigLayout());
+		
+		browse = new JButton("Browse");
+		browse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				File file = FileDialogs.showOpenDialog(DoMosaicsUI.getInstance());
+				if(file != null) {
+					if (file.canRead()) {
+						File test = new File(file.getAbsolutePath());
+						seqs = new FastaReader().getDataFromFile(test);
+						if(seqs != null) {
+							path.setText(file.getAbsolutePath());
+							name.setText(file.getName().split("\\.")[0]);
+
+						}
+					}
+					else {
+						MessageUtil.showWarning("Cannot read "+file.getName());
+						path.setText("");
+					}
+				}
+			}
+		});
+		
+		path = new JTextField();
+		path.setEditable(false);
+		path.setName(FILEPATH_KEY);
+		
+		name = new JTextField();
+		name.setEditable(true);
+		name.setName(VIEWNAME_KEY);
+		
+		// use GUIFactory?
+		ProjectElement[] projectList = WorkspaceManager.getInstance().getProjects();
+		String[] projectNames = new String[projectList.length];
+		for (int i = 0; i < projectList.length; i++) {
+			projectNames[i] = projectList[i].getTitle();
+		}
+		selectProject = new JComboBox(projectNames);
+		selectProject.setName(PROJECTNAME_KEY);
+		selectProject.setSelectedItem(null);
+		
+		add(new JXTitledSeparator("Select view file"), "growx, span, wrap");
+		add(browse, "gapleft 5");
+		add(path, "w 165!, h 25!, span 2, gapright 10, wrap");
+		add(new JLabel("View name:"), "gapleft 5");
+		add(name, "h 25!, w 165!, gapright 10, wrap");
+
+		add(new JXTitledSeparator("Associate with project"), "growx, span, wrap, gaptop 15");
+		add(selectProject, "h 25!, w 265!, gapright 10, span, wrap");
+		
+	}
+	
+	
+    /**
+     * 
+     * Checks if all necessary inputs are made.
+     */
+    protected String validateContents (Component component, Object o) {
+		
+		String newName = name.getText().trim();
+		
+		// check for chosen view path
+		if (path.getText().equals(""))
+			return "Select a view for import";
+		
+		if (newName.equals(""))
+			return "Please choose a name for view";
+		
+		if (newName.length() > 50)
+			return "View name should not exceed 50 characters";
+		
+		// ensure that project is selected
+		if (selectProject.getSelectedItem() == null)
+			return "Please choose a project for association";
+		
+		// get project
+		String projectName = (String) selectProject.getSelectedItem();
+		ProjectElement project = WorkspaceManager.getInstance().getProject(projectName);
+			
+		// ensure that name does not exist
+		if (project.viewExists(newName, project.getCategory(type)))
+			return "Name taken - choose new name";
+
+		return null;
+	}
+
+
+}
+
