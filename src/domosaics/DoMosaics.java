38052e902f7a5854bd159adcbf49a55b9626e852
Andrew Moore <radmoore@gmail.com>
2013-05-20 22:08:41 +0200

    updated exception settings

--- src/domosaics/DoMosaics.java
+++ src/domosaics/DoMosaics.java
@@ -70,7 +70,7 @@ public class DoMosaics {
 	public static void main(String[] args) {
 		
 		Configuration.setDebug(false);
-		Configuration.setReportExceptionsMode(false);
+//		Configuration.setReportExceptionsMode(false);
 		 
 		if (args.length > 0) {
 			for(String a : args) {
