ec9af2199e2a2fbbcb623524e140df4d9d6513ce
Andrew Moore <radmoore@gmail.com>
2013-12-08 01:18:21 +0100

    Updated version string

--- src/domosaics/model/configuration/Configuration.java
+++ src/domosaics/model/configuration/Configuration.java
@@ -27,7 +27,7 @@ public class Configuration {
 	
 //	public static final String sep = System.getProperty("file.separator");
 	
-	public static final double CURRENT_PROGRAM_VERSION = 0.92;
+	public static final double CURRENT_PROGRAM_VERSION = 0.94;
 	
 	public static final String DEF_HOMEFOLDER_LOCATION = System.getProperty("user.home");
 	public static final String DEF_WORKSPACE = DEF_HOMEFOLDER_LOCATION + File.separator + "domosaics_workspace";
